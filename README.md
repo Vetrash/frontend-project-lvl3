c220ff2e9b3900f1bc0b2e2b56189816fa85295d
Vetrash <Vetrash47@gmail.com>
2022-07-29 16:38:42 +0500

    старт работы
    создал шаблон проекта

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vetrash/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/Vetrash/frontend-project-lvl3/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/249e949fa35bbcf4c272/maintainability)](https://codeclimate.com/github/Vetrash/frontend-project-lvl3/maintainability)
+[![Actions Status](https://github.com/Vetrash/frontend-project-lvl3/workflows/auto-test/badge.svg)]
+<a href="https://codeclimate.com/github/Vetrash/frontend-project-lvl3/test_coverage"><img src="https://api.codeclimate.com/v1/badges/249e949fa35bbcf4c272/test_coverage" /></a>
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: `${__dirname}/src/index.js`,
+    output: {
+      path: `${__dirname}/out/`,
+    },
+  mode: process.env.NODE_ENV || 'development',
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },  
+};
