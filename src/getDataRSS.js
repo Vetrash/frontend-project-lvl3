9d2ffcc32c8a6b24c06e3b78bd093ffd5e5337fd
Vetrash <Vetrash47@gmail.com>
2022-08-05 22:11:48 +0500

    добавил прокси All Origins
    добавил прокси All Origins
    Удалил бейджик своих тестов

--- src/getDataRSS.js
+++ src/getDataRSS.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import parserRSS from './parserRSS.js';
 
+const getProxyUrl = (url) => {
+  const proxy = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`;
+  //const feedUrl = new URL(url);
+  //const proxyUrl = new URL(`${feedUrl.host}${feedUrl.pathname}`, proxy);
+  //console.log(proxyUrl.href);
+  //return proxyUrl.href;
+  //console.log(proxy);
+  return proxy;
+};
+
 export default (state) => {
   const st = state;
   const { RSS } = state;
-  const promises = RSS.map((elem) => axios.get(elem)
+  const promises = RSS.map((elem) => axios.get(getProxyUrl(elem))
     .then((res) => {
-      parserRSS(state, res.data);
+      //console.log(res);
+      parserRSS(state, res.data.contents);
     })
     .catch(() => { st.form.log = 'errorLoadOne'; }));
   Promise.all(promises);
